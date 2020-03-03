24b1ded39c2f64f11e31b523c054cfa42c8f0b50
Oleg Höfling <oleg.hoefling@gmail.com>
2020-03-03 01:24:24 +0100

    fix monkeypatching importlib.metadata for python >3.8 in tests
    Signed-off-by: Oleg Höfling <oleg.hoefling@gmail.com>

--- tests/test_version.py
+++ tests/test_version.py
@@ -14,7 +14,9 @@ fake_version = '1.2.3.dev123'
 @pytest.mark.usefixtures('unload_imports')
 def test_version_when_package_installed(monkeypatch):
     # happy testing
-    monkeypatch.setattr('importlib_metadata.version', lambda s: fake_version)
+    monkeypatch.setattr(
+        '{}.version'.format(importlib_metadata.__name__), lambda s: fake_version
+    )
     from devpi_ext import __version__
 
     assert __version__ == fake_version
@@ -25,7 +27,7 @@ def test_version_when_reading_raises(monkeypatch):
     def raise_(*args, **kwargs):
         raise importlib_metadata.PackageNotFoundError()
 
-    monkeypatch.setattr('importlib_metadata.version', raise_)
+    monkeypatch.setattr('{}.version'.format(importlib_metadata.__name__), raise_)
     from devpi_ext import __version__
 
     assert __version__ == 'UNKNOWN'
