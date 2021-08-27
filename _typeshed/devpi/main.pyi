703850e1c509c66b16f7c4a618a24af412e11308
Oleg Höfling <hoefling@users.noreply.github.com>
2021-08-28 00:39:17 +0200

    add precommit.ci integration (#10)

--- _typeshed/devpi/main.pyi
+++ _typeshed/devpi/main.pyi
@@ -1,14 +1,7 @@
-from typing import Any, Callable, overload
+from typing import Any, Callable, TypeVar, overload
+
+from pluggy.hooks import HookimplMarker
+
+hookimpl: HookimplMarker
 
 def __getattr__(name: str) -> Any: ...  # incomplete
-
-AnyFunc = Callable[..., Any]
-@overload
-def hookimpl(function: AnyFunc) -> AnyFunc: ...
-@overload
-def hookimpl(
-    hookwrapper: bool = False,
-    optionalhook: bool = False,
-    tryfirst: bool = False,
-    trylast: bool = False,
-) -> Callable[[AnyFunc], AnyFunc]: ...
